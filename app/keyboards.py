2ad1e0d46680b536a0c045e1d76c0f63c0525cbd
Bayek-Bit <twink.7w1nk@yandex.ru>
2025-01-22 17:37:22 +0500

    keyboards

--- app/keyboards.py
+++ app/keyboards.py
@@ -1,15 +1,17 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
-from app.database import ClientDatabaseHandler
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-ClientHandler = ClientDatabaseHandler()
+def create_categories_keyboard(categories):
+    """Создание клавиатуры с категориями."""
+    keyboard = InlineKeyboardMarkup()
+    for category in categories:
+        keyboard.add(InlineKeyboardButton(text=category['name'], callback_data=f"category_{category['id']}"))
+    return keyboard
 
-main_kb = ReplyKeyboardMarkup(
-    resize_keyboard=True,
-    keyboard=[
-        [KeyboardButton(text="Сделать заказ")],
-        [KeyboardButton(text="История заказов")]
-    ]
-)
-
-categories_kb = ReplyKeyboardMarkup(keyboard=[[]], resize_keyboard=True)
+def create_products_keyboard(products):
+    """Создание клавиатуры с товарами."""
+    keyboard = InlineKeyboardMarkup()
+    for product in products:
+        keyboard.add(InlineKeyboardButton(text=product['name'], callback_data=f"product_{product['id']}"))
+    return keyboard
